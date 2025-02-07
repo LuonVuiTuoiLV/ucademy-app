0da670fc664e1c2760c9e45de7996470c7535b6a
LuonVuiTuoiLV <longvuqn1234567@gmail.com>
2025-02-07 16:08:49 +0700

    deloy webhook

--- src/database/user.model.ts
+++ src/database/user.model.ts
@@ -6,12 +6,12 @@ export interface IUser extends Document {
 	clerkId: string;
 	name: string;
 	username: string;
-	email_address: string;
+	email: string;
 	avatar: string;
 	courses: Schema.Types.ObjectId[];
 	status: EUserStatus;
 	role: EUserRole;
-	createdAt: Date;
+	created_at: Date;
 }
 const userSchema = new Schema<IUser>({
 	clerkId: {
@@ -19,10 +19,10 @@ const userSchema = new Schema<IUser>({
 	},
 	name: { type: String },
 	username: { type: String, unique: true, required: true },
-	email_address: { type: String, unique: true, required: true },
+	email: { type: String, unique: true, required: true },
 	avatar: { type: String },
 	courses: [{ type: Schema.Types.ObjectId, ref: 'Course' }],
-	createdAt: { type: Date, default: Date.now },
+	created_at: { type: Date, default: Date.now },
 	role: {
 		type: String,
 		enum: Object.values(EUserRole),
